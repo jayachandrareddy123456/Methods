bc6899a91e29e6c9a605d4d60e332f6fa0623973
jayachandrareddy123456 <jayachandrareddy123456@gmail.com>
2022-06-15 10:37:27 +0530

    String empty method is upadated@@ Jayachandra

---
+++ javaproject/src/com/jaya/javaproject/StringEmpty.java
@@ -0,0 +1,24 @@
+package com.jaya.javaproject;
+
+public class StringEmpty {
+	public static void main(String[] args) {
+		
+	
+	String name="chaitu";
+	String name1="";
+	String name2="  ";
+	String name3="chaitu is a java developer";
+	System.out.println(name.isEmpty());
+	System.out.println(name1.isEmpty());
+	System.out.println(name2.isEmpty());
+	System.out.println(name3.isEmpty());
+	System.out.println(name.isBlank());
+	System.out.println(name1.isBlank());
+	System.out.println(name2.isBlank());
+	System.out.println(name3.isBlank());
+	String name4="";
+	System.out.println(name4.isBlank());
+	
+	}
+
+}
