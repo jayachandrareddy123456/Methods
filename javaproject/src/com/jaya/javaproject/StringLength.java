fcb02670fc73fa2bba0facc17cd54d98c0135431
jayachandrareddy123456 <jayachandrareddy123456@gmail.com>
2022-06-15 17:53:36 +0530

    added string method that is converting lower to upper case

--- javaproject/src/com/jaya/javaproject/StringLength.java
+++ javaproject/src/com/jaya/javaproject/StringLength.java
@@ -4,6 +4,7 @@ public class StringLength {
 	public static void main(String[] args) {
 		String name="Jayachnadra reddy";
 		String name1=" 12334    ";
+		// this line diaplays the length of the given string
 		System.out.println(name.length());
 		System.out.println(name1.length());
 		
---
+++ javaproject/src/com/jaya/javaproject/UppertoLowerString.java
@@ -0,0 +1,12 @@
+package com.jaya.javaproject;
+
+public class UppertoLowerString {
+	public static void main(String[] args) {
+		String name="Chaitanya";
+		System.out.println(name);
+		
+		System.out.println(name.toUpperCase());
+		
+	}
+
+}
