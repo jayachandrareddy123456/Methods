9633a228f37c1f016947ffdab97123941dcd2ad7
jayachandrareddy123456 <jayachandrareddy123456@gmail.com>
2022-06-15 10:48:40 +0530

    Added one string lenth method @@jayachandra

---
+++ javaproject/src/com/jaya/javaproject/StringLength.java
@@ -0,0 +1,13 @@
+package com.jaya.javaproject;
+
+public class StringLength {
+	public static void main(String[] args) {
+		String name="Jayachnadra reddy";
+		String name1=" 12334    ";
+		System.out.println(name.length());
+		System.out.println(name1.length());
+		
+		
+	}
+
+}
