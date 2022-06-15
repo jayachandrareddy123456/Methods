7d2d91c83117956385160805e468aba2f39ea648
jayachandrareddy123456 <jayachandrareddy123456@gmail.com>
2022-06-15 10:44:19 +0530

    Any one can add some logic to this class

---
+++ javaproject/src/com/jaya/javaproject/StringConcat.java
@@ -0,0 +1,12 @@
+package com.jaya.javaproject;
+
+public class StringConcat {
+	public static void main(String[] args) {
+		String name="chaitu";
+		
+		
+		System.out.println(name.concat(" is a java developer"));
+		
+	}
+
+}
