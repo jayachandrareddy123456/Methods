a738999310ea7b808f5fc0f751780dffa22c3bc1
admin <admin@DESKTOP-FPKR8O2>
2022-06-15 09:03:47 +0530

    developed one string method

---
+++ javaproject/src/com/jaya/javaproject/Gitstringpractice.java
@@ -0,0 +1,15 @@
+package com.jaya.javaproject;
+
+public class Gitstringpractice {
+	public String stringTimes(String str,int n) {
+		String name="";
+		for(int i=0;i<n;i++) {
+			name=name+str;
+			return str;
+		}
+		return name;
+	}
+	public static void main(String[] args) {
+		System.out.println(stringTimes(a,1));
+	}
+}
