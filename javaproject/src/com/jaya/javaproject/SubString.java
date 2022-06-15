e6e0b38eaacf810ce671a2394a94caf9a8b9feff
jayachandrareddy123456 <jayachandrareddy123456@gmail.com>
2022-06-15 07:12:26 +0530

    added one substring methid into project@@Ajayachandra

---
+++ javaproject/src/com/jaya/javaproject/SubString.java
@@ -0,0 +1,13 @@
+package com.jaya.javaproject;
+
+public class SubString {
+	public static void subString() {
+		String name="Chaitu is Java Developer";
+		System.out.println(name.substring(1));
+		
+	}
+	public static void main(String[] args) {
+		subString();
+	}
+
+}
